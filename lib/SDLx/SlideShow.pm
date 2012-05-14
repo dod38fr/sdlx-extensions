ba24cf541fe0ab915ec6c17755d24be79b6d5e2a
Dominique Dumont <dod@debian.org>
2012-05-14 15:47:46 +0200

    perltidy run

--- lib/SDLx/SlideShow.pm
+++ lib/SDLx/SlideShow.pm
@@ -1,92 +1,89 @@
 package SDLx::SlideShow;
 
 use Any::Moose;
-use 5.10.1 ;
+use 5.10.1;
 use Carp;
-use SDL ;
+use SDL;
 use SDLx::Surface;
-use SDL::Color ;
+use SDL::Color;
 
 has slideshow_class => (
-	is => 'rw',
-	isa => 'Str',
-	required => 1,
-	trigger => \&_new_slider,
-); 
+    is       => 'rw',
+    isa      => 'Str',
+    required => 1,
+    trigger  => \&_new_slider,
+);
 
 sub _new_slider {
-	my $self = shift;
-	$self->{slider} = $self->_build_slider ;
+    my $self = shift;
+    $self->{slider} = $self->_build_slider;
 }
 
-has image => ( 
-    is => 'rw', 
-    isa => 'SDLx::Surface',
-    handles => { qw/width w height h/ } ,
+has image => (
+    is       => 'rw',
+    isa      => 'SDLx::Surface',
+    handles  => {qw/width w height h/},
     required => 1,
-    trigger => \&_new_image ,
+    trigger  => \&_new_image,
 );
 
 sub _new_image {
-	my ($self, $new_image, $old_image) = @_;
+    my ( $self, $new_image, $old_image ) = @_;
 
-	return unless defined $old_image ;
+    return unless defined $old_image;
 
-	if ($new_image->w ne $old_image->w or $new_image->h ne $old_image->h) {
-		croak "new image does not match old image size" ;
-	}
-	else {
-		$self->slider->image($new_image) ;
-	}
+    if ( $new_image->w ne $old_image->w or $new_image->h ne $old_image->h ) {
+        croak "new image does not match old image size";
+    }
+    else {
+        $self->slider->image($new_image);
+    }
 }
 
 # slider's tick method will blit on this surface (or create a new one)
-has surface => ( 
-    is => 'ro', 
-    isa => 'SDLx::Surface',
-    lazy => 1  ,
-	builder => '_build_surface' ,
+has surface => (
+    is      => 'ro',
+    isa     => 'SDLx::Surface',
+    lazy    => 1,
+    builder => '_build_surface',
 );
 
 sub _build_surface {
-	my $self = shift;
+    my $self = shift;
 
-    my $s =  SDLx::Surface->new( width => $self->width, height => $self->height); 
+    my $s = SDLx::Surface->new( width => $self->width, height => $self->height );
     SDL::Video::set_color_key( $s, 0, 0 );
-    SDL::Video::set_alpha($s, SDL_RLEACCEL, 0);
-    $self->image->blit($s) ;
-    return $s ;
+    SDL::Video::set_alpha( $s, SDL_RLEACCEL, 0 );
+    $self->image->blit($s);
+    return $s;
 }
 
-
 has 'slider' => (
-	is  => 'ro',
-	isa => 'SDLx::SlideShow::Any',
-	handles => [ qw/tick busy/ ] ,
-	lazy => 1,
-	builder => '_build_slider' ,
+    is      => 'ro',
+    isa     => 'SDLx::SlideShow::Any',
+    handles => [qw/tick busy/],
+    lazy    => 1,
+    builder => '_build_slider',
 );
 
 sub _build_slider {
-	my $self = shift;
-	my $slide_class = $self->slideshow_class ;
-	$slide_class = 'SDLx::SlideShow::'.$slide_class unless $slide_class =~ /^SDLx/ ;
-	
-	my $file_to_load = $slide_class.'.pm' ;
-	$file_to_load =~ s!::!/!g ;
+    my $self        = shift;
+    my $slide_class = $self->slideshow_class;
+    $slide_class = 'SDLx::SlideShow::' . $slide_class unless $slide_class =~ /^SDLx/;
 
-    eval {require $file_to_load ; } ;
+    my $file_to_load = $slide_class . '.pm';
+    $file_to_load =~ s!::!/!g;
+
+    eval { require $file_to_load; };
 
     if ($@) {
-            die "Could not parse $file_to_load: $@\n";
-    } 
-    
-    $slide_class->new( image => $self->image , surface => $self->surface ) ;
+        die "Could not parse $file_to_load: $@\n";
+    }
+
+    $slide_class->new( image => $self->image, surface => $self->surface );
 }
 
-
-__PACKAGE__->meta->make_immutable; # Makes it faster at the cost of startup time
+__PACKAGE__->meta->make_immutable;    # Makes it faster at the cost of startup time
 
 no Moose;
 1;
-
